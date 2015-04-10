b8d16189483884e3a1f8c1ef9bd52b7d816773fe
sidhartha43 <sidhartha43@gmail.com>
2015-04-09 20:53:44 -0500

    using Shadow Dom

--- README.md
+++ README.md
@@ -7,40 +7,20 @@ CSS Switch is a CSS rendered switch to switch between two CSS files :stuck_out_t
 1. Include `css-switch.js` or `css-switch.min.js` in your HTML.
 
         <script src="css-switch.js" type="text/javascript"></script>
-2. Define an element in your HTML that will be dedicate to the switch. For Example:
+2. Declare CSS SWITCH tag in the decument.
 
-        <div id="any-id-here"></div>
+        <css-switch></css-switch>
 *Note: Please leave this element empty as its contents will be replaced by the switch*
 3. Call the initSwitch() function with the following syntax.
 
-        initSwitch('one_css_file', 'another_css_file', 'id/class_of_the_element');
-So, assuming we want to switch between style1.css and style2.css and want the following element to be converted into a switch
-
-        <div id="css-switch"></div>
-Then, add the following code to your HTML (after including `css-switch.js` or `css-switch.min.js`)
+        initSwitch('one_css_file', 'another_css_file', );
+So, assuming we want to switch between style1.css and style2.css Then, add the following code to your HTML (after including `css-switch.js` or `css-switch.min.js`)
 
         <script type="text/javascript">
-          initSwitch("style1.css","style2.css","#css-switch");
-        </script>
-You can also use classes instead of id's
-
-        <div class="css-switch"></div>
-        ...
-        <script src="css-switch.js" type="text/javascript"></script>
-        <script type="text/javascript">
-          initSwitch("style1.css","style2.css",".css-switch");
+          initSwitch("style1.css","style2.css");
         </script>
 
-
-        <script src="css-switch.js" type="text/javascript"></script>
-
-
-        <script type="text/javascript">
-         initSwitch("style1.css","style2.css",".css-switch");
-        </script>
-
-
-    *Please note that in this case, only the first element with the class "css-switch" will be converted into a switch.*
+    *Please note that in this case, only the first element with the tag "css-switch" will be converted into a switch.*
 
 ** Note: Do not include either of the two css files using `<link>` tag. CSS Switch will automatically append the "offCSS" file to your head tag.**
 
--- css-switch.js
+++ css-switch.js
@@ -10,14 +10,14 @@
 var on = 0;
 
 /* function to switch css files */
-function switchCSS(offCSS, onCSS, button){
-	var links = document.getElementsByTagName("link");
+function switchCSS(offCSS, onCSS, button) {
+var links = document.getElementsByTagName("link");
 	for(var i=0; i<links.length; i++){
 		if(on==0){
 			if(links[i].href.indexOf(offCSS)!=-1){
 				links[i].href=onCSS;
 				button.style.background="#56aae8";
-				document.getElementById("inner-switch").style.marginLeft="19px";
+				button.children[0].style.marginLeft="19px";
 				on=1;
 			}
 		}
@@ -25,7 +25,7 @@ function switchCSS(offCSS, onCSS, button){
 			if(links[i].href.indexOf(onCSS)!=-1){
 				links[i].href=offCSS;
 				button.style.background="#ccc";
-				document.getElementById("inner-switch").style.marginLeft="0px";
+				button.children[0].style.marginLeft="0px";
 				on=0;
 			}
 		}
@@ -33,30 +33,37 @@ function switchCSS(offCSS, onCSS, button){
 }
 
 /* function to render css-switch element */
-function initSwitch(offCSS, onCSS, element){
+function initSwitch(offCSS, onCSS){
 	var h = document.getElementsByTagName('head').item(0);
 	var l = document.createElement("link");
 	l.type = "text/css";
 	l.href = offCSS;
 	l.rel = "stylesheet";
 	h.appendChild(l);
-	if(element.charAt(0)=='#'){
-		var button = document.getElementById(element.substr(1));
-	}
-	else if(element.charAt(0)=='.'){
-		var button = document.getElementsByClassName(element.substr(1))[0];
-	}
-	else{
-		console.log("No proper element passed for rendering");
-	}
-	button.style.position="absolute";
-	button.style.width="50px";
-	button.style.height="20px";
-	button.style.background="#ccc";
-	button.style.cursor="pointer";
-	button.style.padding="2.5px";
-	button.style.borderRadius="25px";
-	button.style.boxSizing="border-box";
-	button.innerHTML="<div id='inner-switch' style='position:absolute;width:25px; height:25px; border-radius: 30px;margin-left:0px; margin-top: -5px; transition: all .1s ease-in-out; -webkit-transition: all .1s ease-in-out; background: #f4f4f4;'></div>";
-	button.addEventListener('click', function(){switchCSS(offCSS, onCSS, button);}, false);
+	CreateShadowDom(offCSS,onCSS);
+}
+
+function CreateShadowDom(offCSS,onCSS){
+	var cssSwitch = document.querySelector('css-switch'); 
+	var root = cssSwitch.createShadowRoot();
+
+	var outerdiv = document.createElement('div');
+	outerdiv.setAttribute('id','outer-switch');
+
+	outerdiv.style.position="absolute";
+	outerdiv.style.width="50px";
+	outerdiv.style.height="20px";
+	outerdiv.style.background="#ccc";
+	outerdiv.style.cursor="pointer";
+	outerdiv.style.padding="2.5px";
+	outerdiv.style.borderRadius="25px";
+	outerdiv.style.boxSizing="border-box";
+
+	var div = document.createElement('div');
+	div.setAttribute('id','inner-switch');
+	div.setAttribute('style','position:absolute;width:25px; height:25px; border-radius: 30px;margin-left:0px; margin-top: -5px; transition: all .1s ease-in-out; -webkit-transition: all .1s ease-in-out; background: #f4f4f4;');
+	outerdiv.appendChild(div);
+	root.appendChild(outerdiv);
+
+	outerdiv.addEventListener('click', function(){switchCSS(offCSS,onCSS,this);}, false);	
 }
